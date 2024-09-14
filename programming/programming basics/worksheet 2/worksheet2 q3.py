5d20c771541fb08ad8f0c31436fa9c966f38bf6d
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2024-09-14 12:01:09 +0100

    worksheet 2 and prep 2

---
+++ programming/programming basics/worksheet 2/worksheet2 q3.py
@@ -0,0 +1,17 @@
+orderValue = float(input())
+postageCharge = 0
+totalCharge = 0
+
+if orderValue >= 15:
+    totalCharge += 0 + orderValue
+elif orderValue < 15:
+    totalCharge += 3.5 + orderValue
+
+deliveryType = input("do you want next day delivery")
+if deliveryType == "yes" or "y":
+    totalCharge += 5
+elif deliveryType == "no" or "n":
+    totalCharge = totalCharge
+
+print("the total comes to:", totalCharge)
+    
---
+++ programming/programming basics/worksheet 2/worksheet2 t1q2.py
@@ -0,0 +1,5 @@
+# age = int(input())
+# if age >= 10 and age < 19 then
+#     print("valid pupil age")
+# else
+#     print("invalid input: enter a value from 11 to 18")
