0f4757a7884abc1649c79a9d6f5308ae34a7dc23
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2024-09-20 09:15:36 +0100

    python costructs

---
+++ programming/iteration/python_constructs.py
@@ -0,0 +1,10 @@
+
+# for i in range(1, 13):
+#     s = ""
+#     print(i, "times 5 equals", (5 * i))
+
+# count = 1
+# while count < 13:
+#     print(5 * count)
+#     count += 1
+
--- python_constructs.py
+++
@@ -1,4 +0,0 @@
-
-for i in range(1, 13):
-    s = ""
-    print(i, "times 5 equals", (5 * i))
