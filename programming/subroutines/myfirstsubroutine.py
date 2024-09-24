8955f4515d5010b70ef2b4f712fb75bd88b47ade
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2024-09-24 12:34:36 +0100

    Create myfirstsubroutine.py

---
+++ programming/subroutines/myfirstsubroutine.py
@@ -0,0 +1,15 @@
+def multiplyTwo(x :int, y :int):
+    print("the product is", x*y)
+#end procedure
+multiplyTwo(1,3) 
+
+def proc1(x, y):
+    x = x - 2
+    y = 0
+    print(x * y)
+#end procedure
+
+m = 8
+n = 10
+proc1(m, n)
+print(m, n)
