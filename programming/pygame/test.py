ba5026670c47cb00f66b65747f13a7b3ae03a4f6
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2024-10-08 12:22:50 +0100

    pygame test 1

--- programming/pygame/test.py
+++ programming/pygame/test.py
@@ -1,2 +1,22 @@
 import pygame
 pygame.init()
+done = False
+clock = pygame.time.Clock()
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+title = pygame.display.set_caption("game1")
+
+
+WHITE = (255, 255, 255)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+
+screen.fill(WHITE)
+pygame.display.flip()
+clock.tick(60)
+
+pygame.quit()
