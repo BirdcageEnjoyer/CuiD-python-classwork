6b9b9c0c24b888980559320e13f395359ba3b7e1
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2024-10-11 14:44:49 +0100

    Update test.py

--- programming/pygame/test.py
+++ programming/pygame/test.py
@@ -1,22 +1,70 @@
+
+
+
+# Pygame base template for opening a window
+ 
+#  Sample Python/Pygame Programs
+#  Simpson College Computer Science
+#  http://programarcadegames.com/
+#  http://simpson.edu/computer-science/
+ 
+#  Explanation video: http://youtu.be/vRB_983kUMc
+
+ 
 import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+TEAL = (51, 255, 255)
 pygame.init()
-done = False
-clock = pygame.time.Clock()
+ 
+# Set the width and height of the screen [width, height]
 size = (700, 500)
 screen = pygame.display.set_mode(size)
-title = pygame.display.set_caption("game1")
-
-
-WHITE = (255, 255, 255)
-
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
 while not done:
+    # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(TEAL)
+ 
+    # --- Drawing code should go here
+    
+    pygame.draw.rect(screen, (0, 255, 0), [0, 400, 700, 100])
+    pygame.draw.rect(screen, (255, 0, 255), [400, 250, 200, 150])
+    pygame.draw.line(screen, (0, 0, 0), [350, 250], [500, 180])
+    pygame.draw.line(screen, (0, 0, 0), [500, 180], [650, 250])
+    pygame.draw.line(screen, (0, 0, 0), [350, 250], [650, 250])
 
-
-screen.fill(WHITE)
-pygame.display.flip()
-clock.tick(60)
-
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
 pygame.quit()
