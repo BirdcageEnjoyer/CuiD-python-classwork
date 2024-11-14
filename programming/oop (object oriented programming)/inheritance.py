c314566c7640b6e565a39310c0b4be1690c3e759
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2024-11-14 10:45:10 +0000

    Update inheritance.py

--- programming/oop (object oriented programming)/inheritance.py
+++ programming/oop (object oriented programming)/inheritance.py
@@ -16,12 +16,45 @@ class Animal:
     def move(self):
         pass
     #endprocedure
+    
+    def getType(self):
+        return self.type
+    
+    def getName(self):
+        return self.name
+
+    def getColour(self):
+        return self.colour
 
 
 class Dog(Animal):
-    pass
+    def makeNoise(self):
+        print("woof")
+    #endprocedure
+
+class Cat(Animal):
+    def makeNoise(self):
+        print("meow")
+    #endprocedure
+        
 
 myAnimal = Animal("unknown", "adam", "Yellow")
 
-myAnimal.makeNoise()
-print("end")
+myDog = Dog("dog", "john", "black")
+myCat = Cat("cat", "poo", "red")
+
+animalList = []
+for animal in ["cat", "dog", "dog", "cat", "dog"]:
+    match animal:
+        case "cat": 
+            animalList.append(Cat("mammal", "cat1", "blue2"))
+        case "dog":
+            animalList.append(Dog("mammal", "dog1", "purple2"))
+    #endmatch
+#endfor
+
+for animal in animalList:
+    animal.makeNoise()
+    print(animal.getColour())
+    print(animal.getName())
+    print(animal.getType())
