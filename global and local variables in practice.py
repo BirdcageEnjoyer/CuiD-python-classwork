1bd6f895383c6c8626bda6cd58881a2749171339
BirdcageEnjoyer <CuiD@dulwich.org.uk>
2025-03-17 16:21:37 +0000

    gurobaru wariaburosu

---
+++ global and local variables in practice.py
@@ -0,0 +1,40 @@
+# msg = "hero worudo"
+# print(msg)
+
+# def subroutine():
+#     global msg
+#     msg = "text" # this instead, since global has been said, basically, im a function and i want to use the global "msg" variable, this line instead just changes msg
+#     print(msg)
+
+
+# subroutine()
+# print(msg)
+
+
+
+# array = [5,12]
+
+# print(array)
+
+# def swap():
+#     temp = array[1]
+#     array[1] = array[0]
+#     array[0] = temp
+
+
+# swap()
+# print(array)
+
+
+# for i in range(5):
+#     print("hello")
+
+# print(i)
+
+# n = input().split(", ")
+# print(n)
+
+# n = ["rizzlahs", "ohio", "gyad"]
+# finishedn = ' '.join(n)
+# print(finishedn)
+
